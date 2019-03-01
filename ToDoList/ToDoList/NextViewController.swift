3c53a15b8e21a0ccf943e85fb5cb45ca2f08c7fb
Maripeko <35319748+Maripeko@users.noreply.github.com>
2019-03-01 10:41:43 +0900

    ドラムロールの追加

--- ToDoList/ToDoList/NextViewController.swift
+++ ToDoList/ToDoList/NextViewController.swift
@@ -8,45 +8,72 @@
 import UIKit
 
 class NextViewController: UIViewController {
+ 
+    //カテゴリー入力
+    @IBOutlet weak var CategoryText: UITextField!
     //日付入力
     @IBOutlet weak var DateText: UITextField!
     
     var datePicker: UIDatePicker = UIDatePicker()
+    var categoryPicker: UIPickerView = UIPickerView()
+    let list: [String] = ["個人","仕事","買い物","その他"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //
+        
+        //カテゴリーピッカー設定
+        categoryPicker.delegate = self as? UIPickerViewDelegate
+        categoryPicker.dataSource = self as? UIPickerViewDataSource
+        categoryPicker.showsSelectionIndicator = true
+ 
+        
+        //日付ピッカー設定
         datePicker.datePickerMode = UIDatePicker.Mode.date
         datePicker.timeZone = NSTimeZone.local
         datePicker.locale = Locale.current
         DateText.inputView = datePicker
         
-        //
+        //決定バーの生成
         let wit: Double
         wit = Double(view.frame.size.width)
         let toolbar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: wit, height: 35.0))
         let spaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        let DoneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(ChangeDate)
-            
-        toolbar.setItems([spaceItem, DoneButton], animated: true)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        toolbar.setItems([spaceItem, doneButton], animated: true)
         
-        //
+        //インプットビュー設定
         DateText.inputView = datePicker
         DateText.inputAccessoryView = toolbar
+        
+        CategoryText.inputView = categoryPicker
+        CategoryText.inputAccessoryView = toolbar
+        
+        
+    
+    
         // Do any additional setup after loading the view.
     }
-    //
+    
+    
+    
+    
+    //決定ボタン
     @objc func done(){
-        DateText.endEditing(true)
         
-        //
-    }
-    //
-    @IBAction func ChangeDate(_ sender: UIDatePicker) {
+        //日付のフォーマット
         let formatter = DateFormatter()
-        formatter.dateFormat = "MM月yy日"
-        DateText.text = formatter.string(from: sender.date)
+        formatter.dateFormat = "MM月dd日"
+        DateText.text = "\(formatter.string(from: datePicker.date))"
+        
+        CategoryText.text = "\(list[categoryPicker.selectedRow(inComponent: 0)])"
+        
+        
+        
+        DateText.endEditing(true)
+        CategoryText.endEditing(true)
+        
     }
+
     /*
     // MARK: - Navigation
 
@@ -57,4 +84,31 @@ class NextViewController: UIViewController {
     }
     */
 
+//}
+
+//extension NextViewController : UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    //
+  /*  func numberOfComponents(in categoryPicker: UIPickerView) -> Int {
+        return 1
+    }
+ */
+    
+    //
+    
+  /*  func categoryPicker(_ categoryPicker: UIPickerView, titleForRom row: Int, forcomponent conponent: Int) -> Int {
+      
+        return list.count
+    }
+    
+    func categoryPicker(_ categoryPocker: UIPickerView, titleForRow row: Int, forConponent: Int) -> String? {
+        
+        
+        return list[row]
+    }
+    
+    func categoryPicker(_ categoryPicker: UIPickerView, sisSelectionRow row: Int, inComponent component: Int){
+        CategoryText.text = list[row]
+    }*/
+ 
 }
